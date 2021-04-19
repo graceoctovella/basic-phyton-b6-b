abf23d9bc9209643656af186a03cf38d56a6f4ed
graceoctovella <graceoctovella@gmail.com>
2021-04-20 00:24:38 +0700

    Tugas-1

---
+++ Tugas-1/soal-1.py
@@ -0,0 +1,5 @@
+nama = input ("masukkan nama kamu : ")
+umur = int(input("masukkan umur kamu : "))
+tinggi = float(input("masukkan tinggi kamu : "))
+
+print("Nama saya "+nama+"," " umur saya" ,umur, "tahun, dan tinggi saya" ,tinggi, "cm.")
---
+++ Tugas-1/soal-2.py
@@ -0,0 +1,6 @@
+jari = int(input("masukkan panjang jari-jari lingkaran dalam cm : "))
+
+phi = 22/7
+luas = float(phi*jari*jari)
+#print(luas)
+print("Luas lingkaran dengan jari-jari {} cm adalah {:.2f} cm\u00b2.".format(jari,luas))
